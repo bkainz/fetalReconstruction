f1db79bd73e3db52ebfbb202755dab5e02d51f23
bkainz <b.kainz@imperial.ac.uk>
2016-11-21 09:15:43 +0000

    preparing release of PVR

--- bin/linux64/runTestDataset.sh
+++ bin/linux64/runTestDataset.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./reconstruction_GPU2 -o 3TReconstruction.nii.gz -i ../../data/14_3T_nody_001.nii.gz ../../data/10_3T_nody_001.nii.gz ../../data/21_3T_nody_001.nii.gz ../../data/23_3T_nody_001.nii.gz -m ../../data/mask_10_3T_brain_smooth.nii.gz --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 1.0 
+./SVRreconstructionGPU.exe -o 3TReconstruction.nii.gz -i ../../data/14_3T_nody_001.nii.gz ../../data/10_3T_nody_001.nii.gz ../../data/21_3T_nody_001.nii.gz ../../data/23_3T_nody_001.nii.gz -m ../../data/mask_10_3T_brain_smooth.nii.gz --resolution 1.0 
 
